4cf69a51ec563a382fe3c94812b6adb9dcb19c1e
Mina <nimocanada@gmail.com>
2022-01-27 09:21:54 -0500

    initial commit

---
+++ src/question1/Question1.java
@@ -0,0 +1,45 @@
+/*
+Author: Mina Awad
+Date: January 24 2022
+Assignment Number: 1
+A program to take a four-digit number from input and print it in reverse order
+*/
+package question1;
+
+/**
+ *
+ * @author minaawad
+ */
+public class Question1 
+{
+
+   
+    public static void main(String[] args)
+    {
+        
+        int n = 1;
+        int primeCheck;
+        int counter;
+
+        while (n <= 10000) 
+        {
+            counter = 0;
+            primeCheck = 2;
+            
+            while (primeCheck <= n / 2) 
+            {
+                if (n % primeCheck == 0) 
+                {
+                    counter++;
+                    break;
+                }
+                primeCheck++;
+            }
+            if (counter == 0) 
+            {
+                System.out.print(n + " ");
+            }
+            n++;
+        }     
+    }
+}
