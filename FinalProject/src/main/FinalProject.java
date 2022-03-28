1981bd3d6ed672f913893fc90f20ac9c6e4d71a4
Doyin <Doyin@10.12.186.152>
2022-03-28 17:21:13 -0400

    Bare bones

--- FinalProject/src/main/FinalProject.java
+++ FinalProject/src/main/FinalProject.java
@@ -14,7 +14,9 @@ public class FinalProject {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      AnimationPlayer player = new AnimationPlayer();
+player.loadAnimationFromFile("animation1.txt");
+player.run();
     }
     
 }
