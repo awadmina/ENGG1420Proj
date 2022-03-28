15a5bd877f8a490b375c3ac059798197dad2c83d
Mina <nimocanada@gmail.com>
2022-03-28 17:41:18 -0400

    adding file upload

--- Animationplayerproject/src/ChangeColor.java
+++ Animationplayerproject/src/ChangeColor.java
@@ -1,3 +1,11 @@
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,9 +22,25 @@ public class ChangeColor extends Effects{
         
     }
     
-    public void ChangeColor()
+    public void ChangeColor() throws FileNotFoundException, IOException
+            
     {
         
+        String path="./shapes.txt";
+        
+        File file=new File(path);
+        
+        if(file.exists())
+        {
+         
+            BufferedReader input=new BufferedReader(new FileReader(path));
+            
+            String val = null;
+            while((val=input.readLine())!=null)
+            {
+                System.out.println("Val"+val);
+            }
+        
+        }
     }
-    
 }
--- Animationplayerproject/src/Shape.java
+++ Animationplayerproject/src/Shape.java
@@ -1,3 +1,12 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,13 +18,30 @@
  */
 public class Shape {
     
-    Shape()
-    {
-        
-    }
+   
     
-    public void Shape()
+    public static void main(String... args) throws FileNotFoundException
     {
+        String path="./shapes.txt";
         
+        File file=new File(path);
+        
+        if(file.exists())
+        {
+         
+            BufferedReader input=new BufferedReader(new FileReader(path));
+            
+            String val = null;
+            try {
+                while((val=input.readLine())!=null)
+                {
+                    System.out.println("Val"+val);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Shape.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+        }
+
     }
 }
