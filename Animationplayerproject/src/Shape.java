7416490578868907573dccce118bbaf1ad3c3def
Mina <nimocanada@gmail.com>
2022-03-31 10:24:11 -0400

    added more variables

--- Animationplayerproject/src/Shape.java
+++ Animationplayerproject/src/Shape.java
@@ -28,19 +28,70 @@ public class Shape {
 
             //number of frames
             String numframes = sc.nextLine();
-            System.out.println(numframes);
+            //System.out.println(numframes);
             String[] frames = numframes.split(" ");
             int f = Integer.parseInt(frames[1]);
-            System.out.println(f);
+            System.out.println("Total frames: " + f);
 
             //while (sc.hasNextLine()) {
             //String data = myReader.nextLine();
             //frames per second
             String fps = sc.nextLine();
-            System.out.println(fps);
-            String[] framespersecond = fps.split(" ");
-            int FPS = Integer.parseInt(framespersecond[1]);
-            System.out.println(FPS);
+            //System.out.println(fps);
+            String[] framespersecond = fps.split(" "); //splits string 
+            int FPS = Integer.parseInt(framespersecond[1]); //converts String to number
+            System.out.println("frames per second: " + FPS); //prints the number of fps
+
+            String elements = sc.nextLine();
+            int ELEMENTS = Integer.parseInt(elements);
+            System.out.println("The number of elements: " + ELEMENTS);
+            //String[] ELEMENT = elements.split(" ");
+            //int FPS = Integer.parseInt(framespersecond[1]);
+            //System.out.println(FPS);
+
+            //in circle class
+            String object = sc.nextLine();
+            if (object.contains("Circle")) {        //circle
+                //radius
+                String radius = sc.nextLine();
+                String[] RADIUS = radius.split(" ");
+                int r = Integer.parseInt(RADIUS[1]);
+                System.out.println("r: " + r);
+                //x-axis
+                String x_axis = sc.nextLine();
+                String[] X_AXIS = x_axis.split(" ");
+                int x = Integer.parseInt(X_AXIS[1]);
+                System.out.println("x: " + x);
+                //y-axis
+                String y_axis = sc.nextLine();
+                String[] Y_AXIS = y_axis.split(" ");
+                int y = Integer.parseInt(Y_AXIS[1]);
+                System.out.println("y: " + y);
+                //read show and hide
+                String show = sc.nextLine();
+                
+                if(show.contains("Show")){
+                    String s = sc.nextLine();
+                    String[] START = s.split(" ");
+                    int startshow = Integer.parseInt(START[1]);
+                    System.out.println("Start show: " + startshow);
+                }
+                String hide = sc.nextLine();
+                if (hide.contains("Hide")){
+                    String s2 = sc.nextLine();
+                    String[] START2 = s2.split(" ");
+                    int starthide = Integer.parseInt(START2[1]);
+                    System.out.println("Start hide: " + starthide);
+                }
+                
+
+            } else if (object.contains("Rect")) {
+
+                if (object.contains("Show2")) {    //Show   //there is 2 shows not sure
+                } else if (object.contains("Jump")) {
+                }
+                //add effect and show transfer here
+            }
 
             //System.out.println(data);
             // }
